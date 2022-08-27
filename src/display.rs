9c65b61f3abc819ab6cf37c5e1cba9cf7e434ede
Andrey Kartashov <andrey.kartashov@gmail.com>
2022-08-26 21:14:48 -0400

    display samples/sec

--- src/display.rs
+++ src/display.rs
@@ -8,10 +8,19 @@ use embedded_graphics::{
 use core::fmt::Write;
 use heapless::String;
 
-pub fn render_output<D>(d: &mut D, v: f32) -> Result<(), D::Error>
+// tube recharge time
+const MIN_PERIOD: f32 = 0.0005;
+
+pub fn render_output<D>(d: &mut D, period_secs: f32) -> Result<(), D::Error>
 where
     D: DrawTarget<Color = BinaryColor>,
 {
+    let samples_per_sec = if period_secs < MIN_PERIOD {
+        1.0 / MIN_PERIOD
+    } else {
+        1.0 / period_secs
+    };
+
     let mut sbuf: String<32> = String::new();
 
     let text_style = MonoTextStyleBuilder::new()
@@ -19,9 +28,9 @@ where
         .text_color(BinaryColor::On)
         .build();
 
-    Text::with_baseline("X Duh!", Point::zero(), text_style, Baseline::Top).draw(d)?;
+    Text::with_baseline(">>", Point::zero(), text_style, Baseline::Top).draw(d)?;
 
-    write!(sbuf, "{:6.3}", v);
+    write!(sbuf, "{:6.3}", samples_per_sec).unwrap();
 
     Text::with_baseline(&sbuf, Point::new(0, 16), text_style, Baseline::Top).draw(d)?;
 
